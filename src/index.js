b1ba26c551e5fb9ae8b1f715e3af4ffcf57ccd31
RaivF <122525726+RaivF@users.noreply.github.com>
2024-09-24 18:10:07 +0400

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { StoreContext } from './store/context';
 const store = new Store();
 
 const root = createRoot(document.getElementById('root'));
-
+console.log('123');
 // Первый рендер приложения
 root.render(
   <StoreContext.Provider value={store}>
